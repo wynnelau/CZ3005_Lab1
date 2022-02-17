07e79f6df071022cf1730724f89203d26f70cf41
prolude1 <79588462+prolude1@users.noreply.github.com>
2022-02-17 10:15:03 +0800

    format for printing

--- Algorithm/task_3.py
+++ Algorithm/task_3.py
@@ -1,2 +1,15 @@
-def task_3(G,Coord,Dist,Cost):
-    pass
+def task_3(G, Coord, Dist, Cost):
+    start = "1"
+    end = "50"
+    current_node="1"
+    current_energy=0
+    current_distance=0
+    path=["1"]
+
+
+    if current_node == end and current_energy<=287932:
+        path_string='->'.join([str(elem) for elem in path])
+        print("Shortest path: "+path_string+".")
+        print("Shortest distance: "+current_distance+".")
+        print("Total energy cost: "+current_energy+".")
+        return
--- python main.py
+++ python main.py
@@ -17,8 +17,8 @@ if __name__ == '__main__':
     Cost_data = json.load(Cost)
     Coord = open('Coord.json')
     Coord_data = json.load(Coord)
-    task_1.task_1(G_data,Coord_data,Dist_data,Cost_data)
+    # task_1.task_1(G_data,Coord_data,Dist_data,Cost_data)
     #task_2.task_2(G_data,Coord_data,Dist_data,Cost_data)
-    #task_3.task_3(G_data,Coord_data,Dist_data,Cost_data)
+    task_3.task_3(G_data,Coord_data,Dist_data,Cost_data)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
