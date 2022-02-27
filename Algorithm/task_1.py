3899248ad6fce6d39e3cfbe2ab87c485427a528c
CLAU017 <64260520+Pizza623@users.noreply.github.com>
2022-02-27 21:03:42 +0800

    Update task_1.py
    Should output 
    
    Shortest path: 1->1363->1358->1357->1356->1276->1273->1277->1269->1267->1268->1284->1283->1282->1255->1253->1260->1259->1249->1246->963->964->962->1002->952->1000->998->994->995->996->987->988->979->980->969->977->989->990->991->2369->2366->2340->2338->2339->2333->2334->2329->2029->2027->2019->2022->2000->1996->1997->1993->1992->1989->1984->2001->1900->1875->1874->1965->1963->1964->1923->1944->1945->1938->1937->1939->1935->1931->1934->1673->1675->1674->1837->1671->1828->1825->1817->1815->1634->1814->1813->1632->1631->1742->1741->1740->1739->1591->1689->1585->1584->1688->1579->1679->1677->104->5680->5418->5431->5425->5424->5422->5413->5412->5411->66->5392->5391->5388->5291->5278->5289->5290->5283->5284->5280->50
    
    Shortest distance: 148648.63722140007

--- Algorithm/task_1.py
+++ Algorithm/task_1.py
@@ -1,4 +1,67 @@
 
-
+class Node:
+    def __init__(self, distance, previous_node, visited):
+        self.distance = distance
+        self.previous_node = previous_node
+        self.visited = visited
+        
+    def get_distance(self):
+        return self.distance
+    
+    def get_previous_node(self):
+        return self.previous_node
+    
+    def get_visited(self):
+        return self.visited
+    
+    
 def task_1(G,Coord,Dist,Cost,start,end):
-    pass
+    nodes = {}
+    for i in G:
+        nodes[i]= Node(-1, 'NA', False)
+    current_node = start
+    nodes[current_node] = Node(0, 'NA', True)
+    queue = []
+    
+    while(current_node != end):
+        for i in range(0, len(G[current_node])):
+            if nodes[G[current_node][i]].visited == False:
+                if nodes[G[current_node][i]].distance == -1:
+                    nodes[G[current_node][i]] = Node(Dist[current_node + ',' + G[current_node][i]] + nodes[current_node].distance, current_node, nodes[G[current_node][i]].visited)
+                elif nodes[G[current_node][i]].distance > Dist[current_node + ',' + G[current_node][i]] + nodes[current_node].distance:
+                    nodes[G[current_node][i]] = Node(Dist[current_node + ',' + G[current_node][i]] + nodes[current_node].distance, current_node, nodes[G[current_node][i]].visited)
+        
+        # Put into queue according to distance        
+        for i in range(0, len(G[current_node])):
+            if len(queue) == 0 and nodes[G[current_node][i]].visited == False:
+                queue.append(G[current_node][i])
+            elif nodes[G[current_node][i]].visited == False:
+                queue.append(G[current_node][i])
+                for j in range(len(queue)-1, 0, -1):
+                    if nodes[queue[j]].distance < nodes[queue[j-1]].distance:
+                        swap = queue[j]
+                        queue[j] = queue[j-1]
+                        queue[j-1] = swap
+                    elif queue[j] == queue[j-1]:
+                        queue.remove(queue[j-1])
+                    else:
+                        break 
+                        
+        current_node = queue[0]
+        queue.remove(current_node)
+        nodes[current_node] = Node(nodes[current_node].distance, nodes[current_node].previous_node, True)
+        
+    current_node = end
+    stack = []
+    while(current_node != start):
+        stack.append(nodes[current_node].previous_node)
+        current_node = nodes[current_node].previous_node
+        
+    print("Shortest path:", end = " ")
+    for i in range(len(stack)-1, 0, -1):
+        print(stack[i], end ="->")
+    print(stack[0], end = "->")
+    print(50)
+    
+    print("Shortest distance:", end = " ")
+    print(nodes['50'].distance)
