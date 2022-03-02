b7d28e0093756b2d89f90f2b20ea9fa1a197ce41
ana-17 <63644900+ana-17@users.noreply.github.com>
2022-03-03 00:22:53 +0800

    Update task_2.py

--- Algorithm/task_2.py
+++ Algorithm/task_2.py
@@ -1,174 +1,76 @@
-
-def task_2(G,Coord,Dist,Cost,start,end):
-    task2 = task_2_alg(G,Coord,Dist,Cost,start,end)
-    Edges = {}
-    for y in Cost.keys():
-        Edges[y] = Edge(y)
-    for y in Edges.values():
-        y.set_cost(Cost[y.get_id()])
-        y.set_dist(Dist[y.get_id()])
-    printPath(task2[1], Edges)
-
-
-#function to calculate path, distance, energy cost
-def task_2_alg(G,Coord,Dist,Cost,start,end):
-    from queue import PriorityQueue
-    
-    budget = 287932
-    queue = PriorityQueue()
-    visitedSet = set()
-    
-    
-    Nodes = {}
-    for x in Coord.keys(): 
-        Nodes[x] = Node(x)
-    for x in Nodes.values():
-        x.set_coord(Coord[x.get_id()][0],Coord[x.get_id()][1])
-        x.set_children(G[x.get_id()])
-    
-    
-    Edges = {}
-    for y in Cost.keys():
-        Edges[y] = Edge(y)
-    for y in Edges.values():
-        y.set_cost(Cost[y.get_id()])
-        y.set_dist(Dist[y.get_id()])
-    
-    
-
-    startNode = Nodes[start]
-    queue.put((0,[startNode.get_id()]))
-
-    while not queue.empty():
-        
-        current = queue.get()
-        objs = current[1]
-        nodeCurrent = Nodes[objs[-1]]
-
-        energy = current[0]
-        visitedSet.add(nodeCurrent.get_id())
-
-        if len(objs) <= 0:
-            continue
-        else:
-            if (nodeCurrent.get_id() == end):
-                return current
-            if len(nodeCurrent.get_children()) != 0:
-                for item in nodeCurrent.get_children():
-                    
-                    if not item in visitedSet:
-                        childNode = Nodes[item]
-                        str = nodeCurrent.get_id() + "," + childNode.get_id()
-                        object = Edges[str]
-                        energysum = object.get_cost() + energy
-                        
-                        arr = objs.copy()
-                        arr.append(object.get_ToNode())
-                        if(energysum < budget):
-                            queue.put((energysum, arr))
-            
-    
-    
-    return 0
-
-#function to print path
-def printPath(arrayPath, Edges):
-    
-    if(len(arrayPath) > 1):
-
-        distance = 0 
-        energysum = 0
-        
-        edges = 1
-        tempPath = arrayPath
-
-        pathString = tempPath[0] + "->"
-        while edges < len(tempPath):
-            edgeid = tempPath[edges - 1] + "," + tempPath[edges]
-            edgeobject = Edges.get(edgeid)
-            distance = distance + edgeobject.get_dist()
-            energysum = energysum + edgeobject.get_cost()
-            if edges == len(tempPath)-1:
-                pathString = pathString + tempPath[edges]
-            else:
-                pathString = pathString + tempPath[edges]  + "->"
-            edges+= 1
-            #print(pathStr)
-        pathString = pathString
-
-        print("Shortest path:",pathString)
-        print("Shortest distance:",distance)
-        print("Total energy cost:",energysum)
-    else:
-        pathString = arrayPath[0]
-        print("Shortest path:",pathString)
-        print("Shortest distance:",0)
-        print("Total energy cost:",0)
-
-#############################################################
 class Node:
-        
-
-        childrenObj = []
-        coord = ()
-        children = []
-        id  = ""
-
-        def __init__(self,id):
-            self.id = id
-
-        def get_id(self):
-            return self.id
-
-        def set_coord(self, Xpos, Ypos):
-            self.coord  = (Xpos,Ypos)
-        def get_coord(self):
-            return self.coord
-        
-        def set_children(self,children_list):
-            self.children = children_list
-        def get_children(self):
-            return self.children
-        
-        
-
-
-
-class Edge:    
-    cost = 0
-    dist = 0
-    
-
-    ToObj = None
-    
-    ToNode = ""
-    FromNode = ""
-    
-    id = ""
-
-    def __init__(self,id):
-        self.id = id
-        arr = id.split(',')
-        self.FromNode = arr[0]
-        self.ToNode = arr[1]
-        Edges = {}
-        
-    def get_id(self):
-        return self.id
-    
-
-    def set_cost(self, cost):
+    def __init__(self, distance, previous_node, visited, cost):
+        self.distance = distance
+        self.previous_node = previous_node
+        self.visited = visited
         self.cost = cost
+        
+    def get_distance(self):
+        return self.distance
+    
+    def get_previous_node(self):
+        return self.previous_node
+    
+    def get_visited(self):
+        return self.visited
+    
     def get_cost(self):
         return self.cost
     
-    def set_dist(self, distance):
-        self.dist = distance
-    def get_dist(self):
-        return self.dist
+    
+def task_2(G,Coord,Dist,Cost,start,end):
+    from queue import PriorityQueue
+    energyBudget = 287932
+    nodes = {}
+    for i in G:
+        nodes[i]= Node(-1, 'NA', False, 0)
+    current_node = start
+    dictExplored = {}
+    dictExplored[start] = None
+    nodes[current_node] = Node(0, 'NA', True, 0)
+  
+    q = PriorityQueue()
+    q.put([0, current_node])
+    
+    
+    while(not q.empty()):
+        current_node = q.get()[1]
 
-    def get_FromNode(self):
-        return self.FromNode
-    def get_ToNode(self):
-        return self.ToNode
+        nodes[current_node] = Node(nodes[current_node].distance, nodes[current_node].previous_node, True, nodes[current_node].cost)
+        
+        if(current_node == end):
+            break
+        
+        children = G[current_node]
+        for item in children:
+                   
+            dist = Dist[current_node + "," + item]
+            cost = Cost[current_node + "," + item]
+            
+            newDist = nodes[current_node].distance + dist
+            newCost = nodes[current_node].cost + cost
 
+            if(item not in dictExplored or (newDist<nodes[item].distance)):
+                if(newCost <= energyBudget):
+                    q.put((newDist, item))
+                    dictExplored[item] = current_node
+                    nodes[item] = Node(newDist, current_node, nodes[item].visited, newCost)
+        
+    
+
+    current_node = end
+    stack = []
+    while(current_node != start):
+        stack.append(nodes[current_node].previous_node)
+        current_node = nodes[current_node].previous_node
+        
+    print("Shortest path:", end = " ")
+    for i in range(len(stack)-1, 0, -1):
+        print(stack[i], end ="->")
+    print(stack[0], end = "->")
+    print(50)
+
+    print("Shortest distance:", end = " ")
+    print(nodes[end].distance)
+
+    print("Total energy cost:", nodes[end].cost)
